929baf9e83cb511943189698ad6708b10ff05ca8
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2023-12-03 00:44:28 +0530

    day 1 ques 3

---
+++ Basic Learning/Things To know/IfElseDecisionMaking.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Solution {
+    public static String compareIfElse(int a, int b) {
+        String small =  "smaller";
+        String greater = "greater";
+        String equal = "equal";
+        if(a<b)
+        {
+            return small;
+        }
+        else if(a>b)
+        {
+            return greater;
+        }
+        else{
+            return equal;
+        }
+    }
+}
