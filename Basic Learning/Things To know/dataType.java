8043311e2f80bea837efcd5c738ba6bf78711c37
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2023-12-03 00:37:26 +0530

    day 1 ques 2

---
+++ Basic Learning/Things To know/dataType.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Solution {
+    public static int dataTypes(String type) {
+ 
+        if(type.contains("Integer"))
+        {
+            return 4;
+        }
+        else if(type.contains("Long"))
+        {
+            return 8;
+        }
+        else if(type.contains("Float"))
+        {
+           return 4;
+        }else if(type.contains("Double"))
+        {
+            return 8;
+        }else
+        {
+            return 1;
+        }
+    }
+}
