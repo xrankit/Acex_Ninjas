915b5f65528f3d75b09a3016839083469fb0dd55
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2023-12-05 01:12:34 +0530

    day 4 ques 3

--- Basic Learning/Things To know/switchState.java
+++ Basic Learning/Things To know/switchState.java
@@ -3,13 +3,13 @@ public class switchState {
 
     public static double areaSwitchCase(int ch, double[] a) {
         double area = 0;
-        switch (ch) {
-            case 1:
-                return (22 / 7) * a[0] * a[0];
-            case 2:
-                return a[0] * a[1];
-            default:
-                return area;
+        if (ch == 1) {
+            area = ((22.0 / 7) * a[0] * a[0]);
+            return area;
+        } else if (ch == 2) {
+            return a[0] * a[1];
+        } else {
+            return area;
         }
     }
 }
