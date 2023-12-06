ce06c82a80e47092becc19f3caacdfc6b8939d0e
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2023-12-07 00:16:41 +0530

    days 5 ques 7

---
+++ basicLearning/logicalThinking/pattern2/nNumberTrianglePattern.java
@@ -0,0 +1,15 @@
+package basicLearning.logicalThinking.pattern2;
+
+public class nNumberTrianglePattern {
+    public static void nNumberTriangle(int n) {
+        int num = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num = num + 1;
+            }
+            System.out.println("");
+        }
+    }
+
+}
