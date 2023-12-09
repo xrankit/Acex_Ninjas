e32316213f4ad27b37be9f90094a136cdf50f946
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2023-12-10 01:53:04 +0530

    days 8 ques 3

---
+++ basicLearning/knowbBasicMathematics/calculateHCF.java
@@ -0,0 +1,18 @@
+package basicLearning.knowbBasicMathematics;
+
+public class calculateHCF {
+    public static int calcGCD(int a, int b) {
+        while (a > 0 && b > 0) {
+            if (a > b) {
+                a = a % b;
+            } else {
+                b = b % a;
+            }
+        }
+        if (a == 0) {
+            return b;
+        } else {
+            return a;
+        }
+    }
+}
