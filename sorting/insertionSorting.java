e94f3fe65ebe9b774cdae5b175553f3804c23ff1
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2023-12-20 23:05:36 +0530

    day 18 ques 1

---
+++ sorting/insertionSorting.java
@@ -0,0 +1,15 @@
+package sorting;
+
+public class insertionSorting {
+    public static void insertionSort(int[] arr, int n) {
+        for (int i = 0; i <= n - 1; i++) {
+            int j = i;
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                int temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+    }
+}
