bd844325c979a720ae2489a5df44c44357b749df
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2023-12-21 23:10:17 +0530

    day 19 ques 1

---
+++ array/largestElementInArray.java
@@ -0,0 +1,11 @@
+package array;
+
+import java.util.Arrays;
+
+public class largestElementInArray {
+    static int largestElement(int[] arr, int n) {
+        Arrays.sort(arr);
+        return arr[arr.length - 1];
+
+    }
+}
--- sorting/mergeSorting.java
+++ sorting/mergeSorting.java
@@ -3,13 +3,11 @@ package sorting;
 import java.util.ArrayList;
 
 public class mergeSorting {
-    public static void mergeSort(int[] arr, int low, int high) {
-        int mid = (low + high) / 2;
-        ArrayList<Integer> temp = new ArrayList<>(); // temporary array
-        int left = low; // starting index of left half of arr
-        int right = mid + 1; // starting index of right half of arr
 
-        // storing elements in the temporary array in a sorted manner//
+    private static void merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        int left = low;
+        int right = mid + 1;
 
         while (left <= mid && right <= high) {
             if (arr[left] <= arr[right]) {
@@ -21,26 +19,18 @@ public class mergeSorting {
             }
         }
 
-        // if elements on the left half are still left //
-
         while (left <= mid) {
             temp.add(arr[left]);
             left++;
         }
-
-        // if elements on the right half are still left //
         while (right <= high) {
             temp.add(arr[right]);
             right++;
         }
-
-        // transfering all elements from temporary to arr //
         for (int i = low; i <= high; i++) {
             arr[i] = temp.get(i - low);
         }
     }
-
-}
 
     public static void mergeSort(int[] arr, int low, int high) {
         if (low >= high)
@@ -48,7 +38,7 @@ public class mergeSorting {
         int mid = (low + high) / 2;
         mergeSort(arr, low, mid); // left half
         mergeSort(arr, mid + 1, high); // right half
-        merge(arr, low, mid, high); // merging sorted halves
-    }
+        merge(arr, low, mid, high);
 
-}}}
+    }
+}
