1e347092e819861db6c947e38ca7eef964ffabc7
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2023-12-23 00:00:20 +0530

    commit

---
+++ array/secondLargestFromLastAndFirst.java
@@ -0,0 +1,15 @@
+package array;
+
+public class secondLargestFromLastAndFirst {
+    public static int[] getSecondOrderElements(int n, int[] a) {
+        int[] result = new int[2];
+        // int[] sortedArray = Arrays.copyOf(a, n);
+        // Arrays.sort(sortedArray);
+
+        // result[1] = sortedArray[1];
+
+        // result[0] = sortedArray[n - 2];
+        return result;
+
+    }
+}
