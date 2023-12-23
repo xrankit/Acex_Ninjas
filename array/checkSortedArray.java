13e63118e87f16c6534d7ad0c65c70f5701e1b8a
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2023-12-23 23:14:51 +0530

    day 21 ques1

---
+++ array/checkSortedArray.java
@@ -0,0 +1,12 @@
+package array;
+
+public class checkSortedArray {
+    public static int isSorted(int n, int[] arr) {
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < arr[i - 1])
+                return 0;
+        }
+
+        return 1;
+    }
+}
